c169d4fef23b4d389c6b5a535d38dea534c16fd2
kenta12357 <kenta12357@gmail.com>
2019-12-13 23:32:29 +0900

    change dir

--- GameArticlesMenu/app/src/main/java/com/example/user/gamearticlesmenu/Articles.java
+++
@@ -1,9 +0,0 @@
-package com.example.user.gamearticlesmenu;
-
-public class Articles {
-    public String title = "";
-    public String url = "";
-    public Long date;
-    public boolean isNew = false;
-    public boolean isRead = false;
-}
